59af51379c8f16fa149b848fadd6ac6f6059a373
blees12345 <adriang1702@gmail.com>
2022-05-23 15:02:32 -0700

    add sections and states

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// constants(values that dont change)
+const BABY_IMAGE = ""
+const CHILD_IMAGE = "";
+const TEEN_IMAGE = "";
+const Adult_image = "";
+const MAX_STAT_VALUE = 100; 
+const MAX_BABY_AGE = 5;
+const MAX_CHILD_AGE =19;
+const MAX_TEEN_AGE =19;
+const HUNGER_INTERVAL;
+
+
+// apps state (variables)
+let age;
+let hunger;
+let boredom;
+let sleepiness;
+//dont do this.it might cause bugs
+// let gameOver = hunger > max-stat
+
